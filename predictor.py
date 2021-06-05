9d1d485c1f980e6cebbabaeb356ce1ad9dcb0fd5
g v <appvavass@gmail.com>
2021-06-05 16:49:32 +0200

    separate data regression from prediction function

---
+++ predictor.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#example_input = {'2021-05-23T20:00:00+00:00':25.6,'2021-05-23T21:00:00+00:00':10.4,'2021-05-23T22:00:00+00:00':-10.2}
+
+def forecast_heat_load(input_dict):
+   
+    
+    # Do your forecast here
+
+    forecast = pd.DataFrame.from_dict(data = input_dict, orient= 'index', columns=['forecast_temp'])
+
+    ### use coefficient calculated from the previous data analysis    
+    
+    reg_coefficient = -517.0339
+    reg_intercept = 13246.21239
+    forecast['forecast_load'] = forecast['forecast_temp'].apply(lambda x: reg_coefficient*x + reg_intercept)
+
+    output = forecast['forecast_load'].to_dict
+
+    ### uncomment to plot estimated load
+    
+    #plt.plot(forecast.index, forecast['forecast_load'], color='red')
+    #plt.show()
+    
+
+    return output
--- regression_model.py
+++ regression_model.py
@@ -1,14 +1,9 @@
-import matplotlib
 import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
-
-### small example input dictionary
-example_input = {'2021-05-23T20:00:00+00:00':25.6,'2021-05-23T21:00:00+00:00':10.4,'2021-05-23T22:00:00+00:00':-10.2}
-
 
 conn = sqlite3.connect('city_archive.db')
 
@@ -27,18 +22,18 @@ df_load.drop_duplicates(inplace=True)
 df_temp.drop_duplicates(inplace=True)
 
 hist_df = df_load.merge(df_temp, left_on= 'time', right_on = 'time')
-hist_df['time'] = pd.to_datetime(hist_df['time'])  ##convert to date_time
+hist_df['time'] = pd.to_datetime(hist_df['time'])  ##convert to datetime
 
 ### exploring the data #########################################################
 
 plt.scatter(hist_df['load'],hist_df['temperature'],marker = '*')
-plt.show()
+#plt.show()
 
 ### removing the outlayers to improve dataset for better prediction ############
 
 hist_df_filtered  = hist_df[hist_df['load']>2300]
 plt.scatter(hist_df_filtered['load'],hist_df_filtered['temperature'])
-plt.show()
+#plt.show()
 
 ### preparing a linear regression model
 
@@ -48,18 +43,14 @@ indep_var = hist_df_filtered['temperature'].values.reshape(-1,1)
 linear_regressor = LinearRegression()
 linear_regressor.fit(indep_var,dep_var)
 
+### calculating coefficients for the predicting function to be used
+print(linear_regressor.coef_, linear_regressor.intercept_)
+
+
+### plotting model vs observation to assess quality
 load_predict = linear_regressor.predict(indep_var)
 
 plt.scatter(indep_var,dep_var)
 plt.plot(indep_var, load_predict, color = 'red')
-plt.show()
+#plt.show()
 
-forecast = pd.DataFrame.from_dict(data = example_input, orient= 'index', columns=['forecast_temp'])
-
-temp_forecast  = forecast['forecast_temp'].values.reshape(-1,1)
-forecast['forecast_load'] = linear_regressor.predict(temp_forecast)
-
-plt.plot(forecast.index, forecast['forecast_load'], color = 'red')
-plt.show()
-
-output_dict = forecast['forecast_load'].to_dict
