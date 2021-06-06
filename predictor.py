9d8c7f50b13c9e823cf2cf01226da2537cc72445
g v <appvavass@gmail.com>
2021-06-06 16:12:20 +0200

    minor adjustments

--- predictor.py
+++ predictor.py
@@ -1,11 +1,18 @@
+from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import Type
+from datetime import datetime
+from typing import Dict
+
+temperature =  Type[float]
+heat_load = Type[float]
 
 
-#example_input = {'2021-05-23T20:00:00+00:00':25.6,'2021-05-23T21:00:00+00:00':10.4,'2021-05-23T22:00:00+00:00':-10.2}
+example_input = {'2021-05-23T20:00:00+00:00':25.6,'2021-05-23T21:00:00+00:00':10.4,'2021-05-23T22:00:00+00:00':-10.2}
 
-def forecast_heat_load(input_dict):
+def forecast_heat_load(input_dict: Dict[datetime, temperature]) -> Dict[datetime, heat_load]:
    
     
     # Do your forecast here
@@ -18,7 +25,7 @@ def forecast_heat_load(input_dict):
     reg_intercept = 13246.21239
     forecast['forecast_load'] = forecast['forecast_temp'].apply(lambda x: reg_coefficient*x + reg_intercept)
 
-    output = forecast['forecast_load'].to_dict
+    output_dict = forecast['forecast_load'].to_dict
 
     ### uncomment to plot estimated load
     
@@ -26,4 +33,4 @@ def forecast_heat_load(input_dict):
     #plt.show()
     
 
-    return output
+    return output_dict
